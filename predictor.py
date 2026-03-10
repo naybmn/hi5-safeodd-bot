2a108c3259ca483bdb533078224ab039497f77a2
naybmn <naybomemyatnaing@gmail.com>
2026-03-10 13:50:26 +0630

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -1,35 +1,6 @@
-def safe_prediction(data):
+def live_tip_model(data):
 
-    picks = []
-
-    for match in data["response"]:
-
-        try:
-
-            home = match["teams"]["home"]["name"]
-            away = match["teams"]["away"]["name"]
-
-            status = match["fixture"]["status"]["short"]
-
-            if status != "NS":
-                continue
-
-            tip = home + " Win"
-
-            picks.append((home, away, tip))
-
-            if len(picks) >= 2:
-                break
-
-        except:
-            continue
-
-    return picks
-
-
-def live_under_model(data):
-
-    alerts = []
+    tips = []
 
     for match in data["response"]:
 
@@ -45,39 +16,13 @@ def live_under_model(data):
 
             total_goals = goals_home + goals_away
 
-            if minute >= 60 and total_goals <= 1:
+            if minute >= 60 and total_goals <= 2:
 
-                alerts.append((home, away, minute))
+                tip = "Over 1.5"
 
-        except:
-            continue
-
-    return alerts
-
-
-def value_bet_model(data):
-
-    alerts = []
-
-    for match in data["response"]:
-
-        try:
-
-            minute = match["fixture"]["status"]["elapsed"]
-
-            home = match["teams"]["home"]["name"]
-            away = match["teams"]["away"]["name"]
-
-            goals_home = match["goals"]["home"]
-            goals_away = match["goals"]["away"]
-
-            total_goals = goals_home + goals_away
-
-            if minute >= 55 and total_goals <= 2:
-
-                alerts.append((home, away, minute))
+                tips.append((home, away, minute, tip))
 
         except:
             continue
 
-    return alerts
+    return tips
