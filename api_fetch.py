2e1a051a8d70f0077fc484bbd8d45573ef84a07e
naybmn <naybomemyatnaing@gmail.com>
2026-03-10 13:50:07 +0630

    Update api_fetch.py

--- api_fetch.py
+++ api_fetch.py
@@ -6,15 +6,6 @@ headers = {
     "x-apisports-key": API_KEY
 }
 
-def get_matches():
-
-    url = "https://v3.football.api-sports.io/fixtures?next=50"
-
-    response = requests.get(url, headers=headers)
-
-    return response.json()
-
-
 def get_live_matches():
 
     url = "https://v3.football.api-sports.io/fixtures?live=all"
